ae467562ca7f0652f7dd64f81800776f68ba1536
sourcereris <rimedimas@gmail.com>
2025-09-09 00:40:30 +0900

    GameObject spawn on mouse click
    Spawn GameObject at mouse position using ScriptableObject data

--- Toris/Assets/Scenes/R_TestLevels/R_ScriptableObject/Spawner.cs
+++ Toris/Assets/Scenes/R_TestLevels/R_ScriptableObject/Spawner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Spawner : MonoBehaviour
 {
@@ -12,38 +13,42 @@ public class Spawner : MonoBehaviour
     int instanceNumber = 1;
     private List<GameObject> spawnedEntities = new List<GameObject>();
 
-    private IEnumerator coroutine;
+    Mouse mouse;
 
     void Start()
     {
-        SpawnEntities();
-        coroutine = DestroyEntities(2);
-        StartCoroutine(coroutine);
+        //SpawnEntity();
+        mouse = Mouse.current;
     }
 
-    void SpawnEntities() 
+    void SpawnEntity() 
     {
         int currentSpawnPointIndex = 0;
 
-        for (int i = 0; i < spawnManagerValues.numberOfPrefabsToCreate; ++i) 
+        for (int i = 0; i < spawnManagerValues.pointsList.Count; ++i) 
         {
-            GameObject currentEntity = Instantiate(entityToSpawn, spawnManagerValues.spawnPoints[currentSpawnPointIndex], Quaternion.identity);
+            GameObject currentEntity = Instantiate(entityToSpawn, spawnManagerValues.pointsList[currentSpawnPointIndex], Quaternion.identity);
 
             currentEntity.name = spawnManagerValues.prefabName + instanceNumber;
 
-            currentSpawnPointIndex = (currentSpawnPointIndex + 1) % spawnManagerValues.spawnPoints.Length;
+            currentSpawnPointIndex = (currentSpawnPointIndex + 1) % spawnManagerValues.pointsList.Count;
 
             spawnedEntities.Add(currentEntity);
-
             ++instanceNumber;
         }
     }
-    private IEnumerator DestroyEntities(float time) 
+
+    private void Update()
     {
-        yield return new WaitForSeconds(time);
-        foreach (var entity in spawnedEntities) {
-            Destroy(entity);
+        if (mouse != null && mouse.leftButton.wasPressedThisFrame) 
+        {
+            Vector2 mousePos = Mouse.current.position.ReadValue();
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10f));
+
+            GameObject currentObject = Instantiate(entityToSpawn, worldPos, Quaternion.identity);
+            currentObject.name = spawnManagerValues.name;
+
+            spawnedEntities.Add(currentObject);
         }
     }
-
 }
--- Toris/Assets/Scenes/R_TestLevels/R_ScriptableObject/TreeScriptableObject.cs
+++ Toris/Assets/Scenes/R_TestLevels/R_ScriptableObject/TreeScriptableObject.cs
@@ -1,10 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "TreeScriptableObject", menuName = "Scriptable Objects/TreeScriptableObject")]
 public class TreeScriptableObject : ScriptableObject
 {
     public string prefabName;
-
-    public int numberOfPrefabsToCreate;
-    public Vector3[] spawnPoints;
+    public List<Vector3> pointsList = new();
 }

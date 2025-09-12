02160a575ff7aa7bef5dddd29d3ecb08f3c1f2f4
sourcereris <rimedimas@gmail.com>
2025-09-12 22:04:57 +0900

    Reapply "Collectibles that Follow"
    This reverts commit 14a2fd4a624548b29e8f61d0b0e75afc6cf11242.

--- Toris/Assets/Scenes/R_TestLevels/R_ScriptableObject/Scripts/TreeScriptableObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "TreeScriptableObject", menuName = "Scriptable Objects/TreeScriptableObject")]
-public class TreeScriptableObject : ScriptableObject
-{
-    public string prefabName;
-    public List<Vector3> pointsList = new();
-}

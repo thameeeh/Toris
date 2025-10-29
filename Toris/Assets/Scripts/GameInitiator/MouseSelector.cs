01c876b0c1927859157dfe91cbccf01d7a205c7f
sourcereris <rimedimas@gmail.com>
2025-10-29 09:47:39 +0900

    Menu, procedural Tiles

---
+++ Toris/Assets/Scripts/GameInitiator/MouseSelector.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class MouseSelector : MonoBehaviour
+{
+    private void Start()
+    {
+        SceneManager.LoadSceneAsync("Enemies", LoadSceneMode.Additive);
+    }
+
+    // Update is called once per frame
+
+    private void OnEnable()
+    {
+        
+    }
+    void Update()
+    {
+        if(Mouse.current != null)
+        {
+            return;
+        }
+
+        if(Mouse.current.rightButton.wasPressedThisFrame)
+        {
+            Debug.Log("Right Clicked");
+
+            Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+            if(hit.collider != null)
+            {
+                Debug.Log("Clicked on: " + hit.collider.gameObject.name);
+            }
+        }
+
+    }
+}

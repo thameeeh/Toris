16b2be0f62d1c9fdc27b215676e77ac3b0aff26d
sourcereris <rimedimas@gmail.com>
2025-12-06 16:53:41 +0900

    Refactor UI Toolkit inventory
    Still not working

--- Toris/Assets/UI Toolkit/example_2/InventoryController.cs
+++ Toris/Assets/UI Toolkit/example_2/InventoryController.cs
@@ -1,81 +1,50 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class InventoryController : MonoBehaviour
 {
-    [Header("UI Setup")]
-    [SerializeField] private UIDocument _document;
-
-    // DRAG 'inventory_item.uxml' HERE IN INSPECTOR
-    [SerializeField] private VisualTreeAsset _itemTemplate;
-
-    // Simple Data Class
-    public class ItemData
+    UIDocument UIDocument => GetComponent<UIDocument>();
+    private void Start()
     {
-        public string Name;
-        public Color IconColor; // simulating an icon sprite
-    }
+        VisualElement container = UIDocument.rootVisualElement;
 
-    private List<ItemData> _inventoryData;
-    private ListView _listView;
+        // Create some list of data, here simply numbers in interval [1, 1000]
+        const int itemCount = 1000;
+        var items = new List<string>(itemCount);
+        for (var i = 0; i < itemCount; i++)
+            items.Add(i.ToString());
 
-    void Start()
-    {
-        // 1. Create Fake Data
-        _inventoryData = new List<ItemData>
+        // The "makeItem" function will be called as needed
+        // when the ListView needs more items to render
+        Func<VisualElement> makeItem = () => new Label();
+
+        // As the user scrolls through the list, the ListView object
+        // will recycle elements created by the "makeItem"
+        // and invoke the "bindItem" callback to associate
+        // the element with the matching data item(specified as an index in the list)
+        Action<VisualElement, int> bindItem = (e, i) => ((Label)e).text = items[i];
+
+        var listView = container.Q<ListView>();
+        listView.makeItem = makeItem;
+        listView.bindItem = bindItem;
+        listView.itemsSource = items;
+        listView.selectionType = SelectionType.Multiple;
+
+        // Callback invoked when the user double clicks an item
+        listView.itemsChosen += (selectedItems) =>
         {
-            new ItemData { Name = "Iron Sword", IconColor = Color.red },
-            new ItemData { Name = "Steel Shield", IconColor = Color.blue },
-            new ItemData { Name = "Health Potion", IconColor = Color.green },
-            new ItemData { Name = "Mana Potion", IconColor = Color.cyan },
-            new ItemData { Name = "Old Map", IconColor = Color.yellow }
+            Debug.Log("Items chosen: " + string.Join(", ", selectedItems));
         };
 
-        // 2. Find the ListView
-        var root = _document.rootVisualElement;
-        _listView = root.Q<ListView>("MyInventoryList");
-
-        // 3. Configure the ListView
-
-        // A. Set the Data Source
-        _listView.itemsSource = _inventoryData;
-
-        // B. Set the "makeItem" (Architecture)
-        // If you dragged the UXML to the Inspector, you don't strictly need this line,
-        // but it's good practice to have a code fallback or assign it here explicitly.
-        if (_itemTemplate != null)
+        // Callback invoked when the user changes the selection inside the ListView
+        listView.selectedIndicesChanged += (selectedIndices) =>
         {
-            _listView.makeItem = () => _itemTemplate.Instantiate();
-        }
+            Debug.Log("Index selected: " + string.Join(", ", selectedIndices));
 
-        // C. Set the "bindItem" (The Data Bridge)
-        _listView.bindItem = (VisualElement element, int index) =>
-        {
-            // 'element' is the instantiated 'inventory_item.uxml'
-            var data = _inventoryData[index];
-
-            // Update Text
-            var label = element.Q<Label>("ItemName");
-            label.text = data.Name;
-
-            // Update Icon (Color)
-            var icon = element.Q<VisualElement>("Icon");
-            icon.style.backgroundColor = data.IconColor;
+            // Note: selectedIndices can also be used to get the selected items from the itemsSource directly or
+            // by using listView.viewController.GetItemForIndex(index).
         };
-
-        // D. Optional: Handle Selection
-        _listView.selectionChanged += OnItemSelected;
-    }
-
-    private void OnItemSelected(IEnumerable<object> selectedItems)
-    {
-        // Since we can select multiple, this returns a list. 
-        // We just grab the first one for this example.
-        foreach (var item in selectedItems)
-        {
-            var data = item as ItemData;
-            Debug.Log($"Selected: {data.Name}");
-        }
     }
 }

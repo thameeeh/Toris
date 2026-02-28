123aae9798b0df54a65bc79258906032d9fbe467
sourcereris <rimedimas@gmail.com>
2026-02-28 14:16:54 +0900

    copies of main classes

---
+++ Toris/Assets/UI/Testing_New_Architecture/GameView.cs
@@ -0,0 +1,20 @@
+using OutlandHaven.UIToolkit;
+using UnityEngine.UIElements;
+public abstract class GameView : UIView
+{
+    public abstract ScreenType ID { get; }
+    protected UIEventsSO UIEvents;
+
+    public GameView(VisualElement topElement, UIEventsSO uiEvents) : base(topElement)
+    {
+        UIEvents = uiEvents;
+    }
+
+    public virtual void Setup(object payload) { }
+
+    public override void Show()
+    {
+        base.Show();
+        UIEvents.OnScreenOpen?.Invoke(ID);
+    }
+}
